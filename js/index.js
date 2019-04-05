71d0092a7438a82d8de7ffdb46aaccb136c59b84
dosaem90 <dosaem90@gmail.com>
2019-04-05 18:11:20 +0900

    메뉴바 수정

--- js/index.js
+++ js/index.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
+  menubar();
+  slide();
+});
+
+function slide() {
   $(".bg_img").bxSlider({
     auto: true
   });
-});
+}
+
+function menubar() {
+  $("#nav>ul>li").mouseenter(function() {
+    $("#nav ul ul, .dnav")
+      .stop()
+      .slideDown();
+  });
+  $("#nav>ul>li").mouseleave(function() {
+    $("#nav ul ul , .dnav")
+      .stop()
+      .slideUp();
+  });
+}
