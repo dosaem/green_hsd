b39b4415f09109314d8a0cbd444db90fe0aebbbb
dosaem90 <dosaem90@gmail.com>
2019-05-03 10:07:55 +0900

    tabmenu start

--- js/login.js
+++ js/login.js
@@ -1,17 +1,21 @@
-function form_check(frm) {
-  var uid = document.getElementById("uid");
-  var pwd = document.getElementById("pwd");
+document.addEventListener("DOMContentLoaded", function(event) {
+  let loginBtn = document.getElementById("loginForm");
+  let uid = document.getElementById("uid");
+  let pwd = document.getElementById("pwd");
+  let valid = true;
 
-  if (!uid.value) {
-    alert("아이디를 입력하세요.");
-    uid.focus();
-    return false;
-  }
-  if (!pwd.value) {
-    alert("비밀번호를 입력하세요.");
-    pwd.focus();
-    return false;
-  }
+  loginBtn.addEventListener("submit", form_check, true);
 
-  frm.submit();
-}
+  function form_check(event) {
+    event.preventDefault();
+    if (!uid.value) {
+      uid.focus();
+      return false;
+    }
+    if (!pwd.value) {
+      pwd.focus();
+      return false;
+    }
+    if (valid) this.submit();
+  }
+});
