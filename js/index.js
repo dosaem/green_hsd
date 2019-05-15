bbe3001151216538e442b41c429d33f26e1f5d7b
dosaem90 <dosaem90@gmail.com>
2019-05-16 01:53:32 +0900

    ajax tab test start

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
-  let mapBtn = document.getElementById("mapBtn");
-  // mapBtn.addEventListener("click", submit);
   menubar();
   slide();
   tabmenu();
   searchStore();
+  menuScroll();
 });
 
 function slide() {
@@ -44,4 +43,66 @@ function searchStore() {
   $("#store_search").autocomplete({
     source: availableTags
   });
+}
+// function menuScroll() {
+//   $(window).scroll(function() {
+//     var height = $(document).scrollTop();
+//     if (height > 166) {
+//       console.log(height);
+//     }
+//   });
+//   var lastScrollTop = 0;
+//   $(document).bind("scroll", function(e) {
+//     var st = $(this).scrollTop();
+//     if (st > lastScrollTop) {
+//       console.log("down");
+//     } else {
+//       console.log("up");
+//     }
+//     lastScrollTop = st;
+//   });
+// }
+
+function menuScroll() {
+  // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $("header").outerHeight();
+
+  $(window).scroll(function(event) {
+    didScroll = true;
+  });
+
+  setInterval(function() {
+    if (didScroll) {
+      hasScrolled();
+      didScroll = false;
+    }
+  }, 250);
+
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if (Math.abs(lastScrollTop - st) <= delta) return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight) {
+      // Scroll Down
+      $("#nav")
+        .removeClass("fix")
+        .addClass("nav");
+    } else {
+      // Scroll Up
+      if (st + $(window).height() < $(document).height()) {
+        $("#nav")
+          .removeClass("nav")
+          .addClass("fix");
+      }
+    }
+
+    lastScrollTop = st;
+  }
 }
