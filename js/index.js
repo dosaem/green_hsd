b054d0f7dd05c3c81b20b65342bcaa327047251d
dosaem <40893349+dosaem@users.noreply.github.com>
2019-07-18 21:59:04 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -83,12 +83,7 @@ function menuScroll() {
 
   function hasScrolled() {
     var st = $(this).scrollTop();
-
-    // Make sure they scroll more than delta
     if (Math.abs(lastScrollTop - st) <= delta) return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
     if (st > lastScrollTop && st > navbarHeight) {
       // Scroll Down
       $("#nav")
